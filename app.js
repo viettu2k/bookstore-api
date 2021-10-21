e04c5b19a858706715d351d2a357ba560db8c31e
viettu2k <doanviettu2k@gmail.com>
2021-10-21 10:13:58 +0700

    adding user signup validator

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
 require("dotenv").config();
 
 // import routes
@@ -22,6 +23,7 @@ app.use(
     })
 );
 app.use(cookieParser());
+app.use(expressValidator());
 
 // routes middleware
 app.use("/api", userRoutes);
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { signup } = require("../controllers/user");
+const { userSignupValidator } = require("../validator");
 
-router.get("/signup", signup);
+router.get("/signup", userSignupValidator, signup);
 
 module.exports = router;
