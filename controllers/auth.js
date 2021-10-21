53c79a2ab1040b1d62832768e2f55ced87c17466
viettu2k <doanviettu2k@gmail.com>
2021-10-21 17:52:47 +0700

    adding isAuth and isAdmin middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -56,3 +56,23 @@ exports.requireSignin = expressJwt({
     algorithms: ["HS256"],
     userProperty: "auth",
 });
+
+exports.isAuth = (req, res, next) => {
+    let user = req.profile && req.auth && req.profile._id == req.auth._id;
+    console.log("user", user);
+    if (!user) {
+        return res.status(403).json({
+            error: "Access denied!",
+        });
+    }
+    next();
+};
+
+exports.isAdmin = (req, res, next) => {
+    if (req.profile.role === 0) {
+        return res.status(403).json({
+            error: "Admin resource! Access denied!",
+        });
+    }
+    next();
+};
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { requireSignin } = require("../controllers/auth");
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById, read } = require("../controllers/user");
 
-router.get("/users/:userId", requireSignin, read);
+router.get("/users/:userId", requireSignin, isAuth, read);
 
 router.param("userId", userById);
 
