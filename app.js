55c37d1d36a010020b59fcdb97d651a283098c05
viettu2k <doanviettu2k@gmail.com>
2021-10-21 08:49:37 +0700

    adding user routes

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+// import routes
+const userRoutes = require("./routes/user");
+
 // app
 const app = express();
 
 // connect db
 mongoose.connect(process.env.DATABASE).then(() => console.log("DB connected"));
 
-// routes
-app.get("/", (req, res) => {
-    res.send("hello from node");
-});
+// routes middleware
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000;
 
