3104f8665639da91ea78a87584880fcec4755515
viettu2k <doanviettu2k@gmail.com>
2021-10-21 09:20:24 +0700

    adding password virtual field

--- models/user.js
+++ models/user.js
@@ -33,3 +33,28 @@ const userSchema = new mongoose.Schema({
         default: [],
     },
 }, { timestamps: true });
+
+// virtual field
+userSchema
+    .virtual("password")
+    .set(function(password) {
+        this._password = password;
+        this.salt = uuidv1();
+        this.hash_password = this.encryptPassword(password);
+    })
+    .get(function() {
+        return this._password;
+    });
+
+userSchema.methods = {
+    encryptPassword: function(password) {
+        if (!password) return "";
+        try {
+            return crypto.createHmac("sha1", this.salt).update(password).digest(hex);
+        } catch (err) {
+            return "";
+        }
+    },
+};
+
+module.exports = mongoose.model("User", userSchema);
