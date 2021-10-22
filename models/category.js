f979f2b2109f7ee8d064d62d96fa5caa86ccdbe4
viettu2k <doanviettu2k@gmail.com>
2021-10-22 09:01:06 +0700

    adding category route, model and controllers

---
+++ models/category.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        require: true,
+        maxLength: 32,
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model("Category", categorySchema);
