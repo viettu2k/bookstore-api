9b972aac1614756a7765daa427a21b0fe12df434
viettu2k <doanviettu2k@gmail.com>
2021-10-22 15:25:33 +0700

    adding product delete

--- controllers/product.js
+++ controllers/product.js
@@ -64,9 +64,21 @@ exports.create = (req, res) => {
 
         product.save((err, result) => {
             if (err) {
-                return res.status(400), json({ error: errorHandler(err) });
+                return res.status(400).json({ error: errorHandler(err) });
             }
             res.json(result);
+        });
+    });
+};
+
+exports.remove = (req, res) => {
+    let product = req.product;
+    product.remove((err, deletedProduct) => {
+        if (err) {
+            return res.status(400).json({ error: errorHandler(err) });
+        }
+        res.json({
+            message: "Product deleted successfully",
         });
     });
 };
--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read } = require("../controllers/product");
-const { requireSignin, isAuth } = require("../controllers/auth");
+const { create, productById, read, remove } = require("../controllers/product");
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
 router.get("/products/:productId", read);
-router.post("/products/create/:userId", requireSignin, isAuth, create);
+router.post("/products/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.delete(
+    "/products/:productId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    remove
+);
 
 router.param("userId", userById);
 router.param("productId", productById);
