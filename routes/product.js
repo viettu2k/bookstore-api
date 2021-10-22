eddf91a1ed3442974862add63370a8a9e343b803
viettu2k <doanviettu2k@gmail.com>
2021-10-22 15:16:15 +0700

    adding productById middleware

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/product");
+const { create, productById, read } = require("../controllers/product");
 const { requireSignin, isAuth } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
+router.get("/products/:productId", read);
 router.post("/products/create/:userId", requireSignin, isAuth, create);
 
 router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
