d83328d9e48b4732e631278fe53d0097fe6a4216
viettu2k <doanviettu2k@gmail.com>
2021-10-22 16:05:08 +0700

    adding categoryById and get single category

--- routes/category.js
+++ routes/category.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/category");
+const { create, categoryById, read } = require("../controllers/category");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
+router.get("/categories/:categoryId", read);
 router.post(
     "/categories/create/:userId",
     requireSignin,
@@ -14,5 +15,6 @@ router.post(
 );
 
 router.param("userId", userById);
+router.param("categoryId", categoryById);
 
 module.exports = router;
