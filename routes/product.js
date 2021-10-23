c071a6fd17e4fbadef6655565f93f427fb0f2aa6
viettu2k <doanviettu2k@gmail.com>
2021-10-23 10:51:28 +0700

    adding product list by search

--- routes/product.js
+++ routes/product.js
@@ -10,9 +10,15 @@ const {
     list,
     listRelated,
     listCategories,
+    listBySearch,
 } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
+
+router.get("/products", list);
+router.get("/products/related/:productId", listRelated);
+router.get("/products/categories", listCategories);
+router.post("/products/by/search", listBySearch);
 
 router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
@@ -34,6 +40,7 @@ router.put(
 router.get("/products", list);
 router.get("/products/related/:productId", listRelated);
 router.get("/products/categories", listCategories);
+router.post("/products/by/search", listBySearch);
 
 router.param("userId", userById);
 router.param("productId", productById);
