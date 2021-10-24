1837bf2dbf14fecee3e70eda32ffb43825c27c43
viettu2k <doanviettu2k@gmail.com>
2021-10-24 20:31:35 +0700

    adding get product photo by id

--- controllers/product.js
+++ controllers/product.js
@@ -252,3 +252,11 @@ exports.listBySearch = (req, res) => {
             });
         });
 };
+
+exports.photo = (req, res, next) => {
+    if (req.product.photo) {
+        res.set("Content-Type", req.product.photo.contentType);
+        return res.send(req.product.photo.data);
+    }
+    next();
+};
--- routes/product.js
+++ routes/product.js
@@ -11,6 +11,7 @@ const {
     listRelated,
     listCategories,
     listBySearch,
+    photo,
 } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
@@ -19,6 +20,7 @@ router.get("/products", list);
 router.get("/products/related/:productId", listRelated);
 router.get("/products/categories", listCategories);
 router.post("/products/by/search", listBySearch);
+router.get("/product/photo/:productId", photo);
 
 router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
