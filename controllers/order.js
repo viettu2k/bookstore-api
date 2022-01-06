9bbf4feb96f25adf272311b2d882a2aebd6f9edb
viettu2k <doanviettu2k@gmail.com>
2022-01-06 22:57:27 +0700

    update

--- controllers/order.js
+++ controllers/order.js
@@ -2,7 +2,6 @@ const { Order, CartItem } = require("../models/order");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.create = (req, res) => {
-    // console.log("create order: ", req.body);
     req.body.order.user = req.profile;
     const order = new Order(req.body.order);
     order.save((error, data) => {
